15df0e0344d842b17a3a644909060bb2c137117f
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2024-12-06 18:40:18 +0000

    Linked_list_Insertions

---
+++ linked_list_instertion.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class linked{
+    public static void main(String [] k){
+
+        LinkedList<String> list = new LinkedList<String>();
+
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("E");
+
+        System.out.println(list);
+
+    }
+}
