bc4f7379092ab67639cd932b279c44283f0876f9
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2024-12-29 20:15:52 +0530

    Create NestedLoop8.java

---
+++ NestedLoop8.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main{
+    public static void main(String [] k){
+        
+        Scanner s1 = new Scanner(System.in);
+        
+        int n = s1.nextInt();
+        
+        for(int i = 1; i <= n; i++){
+            
+            for(int j = i; j >= 1; j = j - 1){
+                System.out.print(j + " ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
