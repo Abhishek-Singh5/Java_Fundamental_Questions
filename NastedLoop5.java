3a6f66aa8d033c1421bfdeca3a21d2875af35014
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2024-12-29 19:50:17 +0530

    Create NastedLoop5.java

---
+++ NastedLoop5.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Main{
+    public static void main(String [] k){
+        
+        Scanner s1 = new Scanner(System.in);
+        
+        int n = s1.nextInt();
+        int space = n - 1;
+        int star = 1;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= space; j = j + 1){
+                System.out.print(" ");
+            }
+            
+            for(int j = 1; j <= star; j++){
+                System.out.print("* ");
+            }
+            
+            space --;
+            star ++;
+            
+            System.out.println();
+        }
+    }
+}
