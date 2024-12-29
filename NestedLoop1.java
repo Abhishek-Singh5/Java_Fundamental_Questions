b4c2ae2c8d35d5f2c14678a874c62db57c527f4c
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2024-12-29 14:20:53 +0530

    Create NestedLoop1.java

---
+++ NestedLoop1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main{
+    public static void main(String [] k){
+        
+        Scanner s1 = new Scanner(System.in);
+        
+        int num = 3;
+        
+        for(int i = 1; i <= 3; i++){
+            for(int j = 1; j <= 3; j = j + 1){
+                System.out.print(" Hello ");
+            }
+            
+            System.out.println();
+        }
+        
+    }
+}
