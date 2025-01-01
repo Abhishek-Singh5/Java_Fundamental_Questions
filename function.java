e582e46df2c376adcb615969406499f87e6feac1
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2025-01-02 02:42:04 +0530

    Create function.java

---
+++ function.java
@@ -0,0 +1,18 @@
+class Main{
+    
+    public static int sum(int x, int y){
+        int add = x + y;
+        
+        return add;
+    }
+
+    public static void main(String [] k){
+        
+        int a = 5;
+        int b = 6;
+        
+        int result = sum(a, b);
+        
+        System.out.println(result);
+    }
+}
