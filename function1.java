792f63eb6ea3d10dea97d10594011e8a5dc80b28
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2025-01-02 02:47:01 +0530

    Create function1.java

---
+++ function1.java
@@ -0,0 +1,17 @@
+class Main{
+    
+    public static void sum(int x, int y, int z){
+        int add = x + y;
+        
+        System.out.println(add);
+    }
+
+    public static void main(String [] k){
+        
+        int a = 5;
+        int b = 6;
+        int c = 8;
+        
+        sum(a, b, c);
+    }
+}
