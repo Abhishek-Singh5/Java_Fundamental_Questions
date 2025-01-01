934785e60637df8ace5365e2ef1d0e98eb60ee43
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2025-01-02 02:56:46 +0530

    Create function2.java

---
+++ function2.java
@@ -0,0 +1,18 @@
+class Main{
+    
+    public static double product(double x, double y){
+        double multiply = x * y;
+        
+        return multiply;
+    }
+
+    public static void main(String [] k){
+        
+        double a = 5;
+        double b = 6;
+
+        
+        double result = product(a, b);
+        System.out.println(result);
+    }
+}
