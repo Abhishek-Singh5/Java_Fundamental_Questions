bc285bb1babd355afae5217629954e506b08d3c9
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2025-01-01 23:55:55 +0530

    Update CubeDice.java

--- CubeDice.java
+++ CubeDice.java
@@ -2,11 +2,16 @@ import java.util.*;
 class Main {
     public static void main(String [] k){
         Scanner s1 = new Scanner(System.in);
-        System.out.println("Enter the face value of Dice :- ");
+        System.out.println("Enter the face value of Dice Between 1 to 6:- ");
         int num = s1.nextInt();
+        
+        if(num < 1 || num > 6){
+            System.out.println("Invalid Number");
+        }else{
         
         int dice = 7 - num;
         System.out.println("Opposite face of the Dice is :- \n" + dice);
+        }
     
     }
 }
