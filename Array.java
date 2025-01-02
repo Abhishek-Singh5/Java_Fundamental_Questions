b742461d0ac72199efd6fce8820ad4670a3541d2
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2025-01-03 02:03:41 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main{
+    public static void main(String [] k){
+        int arr[] = new int[5];
+        
+        arr[0] = 5;
+        arr[1] = 7;
+        arr[2] = 9;
+        arr[3] = 3;
+        arr[4] = 4; 
+        
+        for(int i = 0; i < 5; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
